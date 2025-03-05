d552764d4070c4a78bc715c116ea8ae80906e3b5
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-03-05 10:44:50 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+//Name : Aneeka Jain
+//Prn no. : 23070126013
+//AIML A1
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Menu-driven program for deck operations
+        Scanner scanner = new Scanner(System.in);
+        Deck deck = new Deck();
